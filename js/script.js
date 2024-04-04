f17f549af0c0d102536f1821e6987788db2dbebe
sashapoliti <sas.politi@gmail.com>
2024-04-04 22:53:13 +0200

    bonus - add delete chat, need to fix the case when all the chats are gone

--- js/script.js
+++ js/script.js
@@ -8,13 +8,13 @@ createApp({
   data() {
     return {
       contacts,
-      activeID : contacts[0].id,
-      newMessage : '',
-      filterText : '',
-      focusInputChat : false,
-      contactStatus : 'offline',
-      deleteAllDropdown : false
-    }
+      activeID: contacts[0].id,
+      newMessage: "",
+      filterText: "",
+      focusInputChat: false,
+      contactStatus: "offline",
+      deleteAllDropdown: false,
+    };
   },
   methods: {
     changeContact(id) {
@@ -22,63 +22,85 @@ createApp({
     },
     createMessage(message, status) {
       const myMessage = {
-        date: dateTime.now().setLocale('it').toFormat('HH:mm'),
+        date: dateTime.now().setLocale("it").toFormat("HH:mm"),
         message,
         status,
-        showDropdown: false
+        showDropdown: false,
       }; /* var for a new message sent */
       return myMessage;
     },
     addMessage() {
       /* console.log('yes'); */
-      if (this.newMessage.trim() !== '') {
-        this.activeContact.messages.push(this.createMessage(this.newMessage, 'sent'));
-        this.contactStatus = 'writing';
+      if (this.newMessage.trim() !== "") {
+        this.activeContact.messages.push(
+          this.createMessage(this.newMessage, "sent")
+        );
+        this.contactStatus = "writing";
 
         setTimeout(() => {
-          this.activeContact.messages.push(this.createMessage('ok', 'received'));
-          this.contactStatus = 'online';
+          this.activeContact.messages.push(
+            this.createMessage("ok", "received")
+          );
+          this.contactStatus = "online";
         }, 2000); /* timeout for a reply after 1 second */
 
         setTimeout(() => {
-          this.contactStatus = 'offline';
+          this.contactStatus = "offline";
         }, 5000); /* timeout for reset status offline */
       }
 
-      this.newMessage = '';
+      this.newMessage = "";
     },
     showDeleteDropdown(index) {
       /* console.log('im here'); */
       this.deleteAllDropdown = false; //reset header dropdown
       if (this.activeContact.messages[index].showDropdown === true) {
-        this.activeContact.messages[index].showDropdown = false; /* reset dropdown if u click again */
+        this.activeContact.messages[
+          index
+        ].showDropdown = false; /* reset dropdown if u click again */
       } else {
-        this.activeContact.messages.forEach((message) => message.showDropdown = false); //reset for close every other dropdown in the chat
+        this.activeContact.messages.forEach(
+          (message) => (message.showDropdown = false)
+        ); //reset for close every other dropdown in the chat
         this.activeContact.messages[index].showDropdown = true;
       }
     },
     deleteMessage(index) {
-      this.activeContact.messages.splice(index, 1); /* splice the message with index */
+      this.activeContact.messages.splice(
+        index,
+        1
+      ); /* splice the message with index */
     },
     sendButton() {
       this.focusInputChat = !this.focusInputChat;
     },
     showDeleteAllDropdown() {
       this.deleteAllDropdown = !this.deleteAllDropdown;
-      this.activeContact.messages.forEach((message) => message.showDropdown = false); //reset for close every other dropdown in the chat
+      this.activeContact.messages.forEach(
+        (message) => (message.showDropdown = false)
+      ); //reset for close every other dropdown in the chat
     },
     deleteAllMessage() {
       this.activeContact.messages = [];
       this.deleteAllDropdown = false; //reset header dropdown
-    }
+    },
+    deleteChat() {
+      const currentIndex = this.contacts.findIndex((contact) => contact.id === this.activeID); /* find index of active contact */
+      this.contacts.splice(currentIndex, 1); // delete contact
+
+      this.activeID = (currentIndex > this.contacts.length - 1) ? this.contacts[currentIndex - 1].id : this.contacts[currentIndex].id; //set new active id with same index or index - 1 for last element of array
+      this.deleteAllDropdown = false; //reset header dropdown
+    },
   },
   computed: {
     activeContact() {
       return this.contacts.find((element) => element.id === this.activeID);
     },
     filterContacts() {
-      return this.contacts.filter((element) => element.name.toLowerCase().includes(this.filterText.toLowerCase()));
-    }
+      return this.contacts.filter((element) =>
+        element.name.toLowerCase().includes(this.filterText.toLowerCase())
+      );
+    },
   },
   mounted() {
     console.log(this.contacts);
